78732735a634bf33476b329e4debb60fda7454f1
Vinicius Campos <vcampos.pitangui@gmail.com>
2019-11-11 17:12:07 -0300

    Add tests for win condition to player 1 and player 2

---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,132 @@
+# frozen_string_literal: true
+
+require_relative('../lib/player.rb')
+require_relative('../lib/board.rb')
+require_relative('../lib/game.rb')
+
+RSpec.describe 'Tic_Tac_Toe' do
+  let(:game) { Game.new }
+  
+  describe 'Win Conditions' do
+    context 'Player wins' do
+      context 'Rows' do
+        it 'Check win first row for player 1' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(1, game.board.player1.marker)
+          game.board.set_marker(2, game.board.player1.marker)
+          game.board.set_marker(3, game.board.player1.marker)
+          expect(game.winner?(game.board.player1)).to eql(true)
+        end
+        it 'Check win second row for player 1' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(4, game.board.player1.marker)
+          game.board.set_marker(5, game.board.player1.marker)
+          game.board.set_marker(6, game.board.player1.marker)
+          expect(game.winner?(game.board.player1)).to eql(true)
+        end
+        it 'Check win third row for player 1' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(7, game.board.player1.marker)
+          game.board.set_marker(8, game.board.player1.marker)
+          game.board.set_marker(9, game.board.player1.marker)
+          expect(game.winner?(game.board.player1)).to eql(true)
+        end
+        it 'Check win first row for player 2' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(1, game.board.player2.marker)
+          game.board.set_marker(2, game.board.player2.marker)
+          game.board.set_marker(3, game.board.player2.marker)
+          expect(game.winner?(game.board.player2)).to eql(true)
+        end
+        it 'Check win second row for player 2' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(4, game.board.player2.marker)
+          game.board.set_marker(5, game.board.player2.marker)
+          game.board.set_marker(6, game.board.player2.marker)
+          expect(game.winner?(game.board.player2)).to eql(true)
+        end
+        it 'Check win third row for player 2' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(7, game.board.player2.marker)
+          game.board.set_marker(8, game.board.player2.marker)
+          game.board.set_marker(9, game.board.player2.marker)
+          expect(game.winner?(game.board.player2)).to eql(true)
+        end
+      end
+      context 'Columns' do
+        it 'Check win first column for player 1' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(1, game.board.player1.marker)
+          game.board.set_marker(4, game.board.player1.marker)
+          game.board.set_marker(7, game.board.player1.marker)
+          expect(game.winner?(game.board.player1)).to eql(true)
+        end
+        it 'Check win second column for player 1' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(2, game.board.player1.marker)
+          game.board.set_marker(5, game.board.player1.marker)
+          game.board.set_marker(8, game.board.player1.marker)
+          expect(game.winner?(game.board.player1)).to eql(true)
+        end
+        it 'Check win third column for player 1' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(3, game.board.player1.marker)
+          game.board.set_marker(6, game.board.player1.marker)
+          game.board.set_marker(9, game.board.player1.marker)
+          expect(game.winner?(game.board.player1)).to eql(true)
+        end
+        it 'Check win first column for player 2' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(1, game.board.player2.marker)
+          game.board.set_marker(4, game.board.player2.marker)
+          game.board.set_marker(7, game.board.player2.marker)
+          expect(game.winner?(game.board.player2)).to eql(true)
+        end
+        it 'Check win second column for player 2' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(2, game.board.player2.marker)
+          game.board.set_marker(5, game.board.player2.marker)
+          game.board.set_marker(8, game.board.player2.marker)
+          expect(game.winner?(game.board.player2)).to eql(true)
+        end
+        it 'Check win third column for player 2' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(3, game.board.player2.marker)
+          game.board.set_marker(6, game.board.player2.marker)
+          game.board.set_marker(9, game.board.player2.marker)
+          expect(game.winner?(game.board.player2)).to eql(true)
+        end
+      end
+      context 'Diagonals' do
+        it 'Check win first diagonal for player 1' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(1, game.board.player1.marker)
+          game.board.set_marker(5, game.board.player1.marker)
+          game.board.set_marker(9, game.board.player1.marker)
+          expect(game.winner?(game.board.player1)).to eql(true)
+        end
+        it 'Check win second diagonal for player 1' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(3, game.board.player1.marker)
+          game.board.set_marker(5, game.board.player1.marker)
+          game.board.set_marker(7, game.board.player1.marker)
+          expect(game.winner?(game.board.player1)).to eql(true)
+        end
+        it 'Check win first diagonal for player 2' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(1, game.board.player2.marker)
+          game.board.set_marker(5, game.board.player2.marker)
+          game.board.set_marker(9, game.board.player2.marker)
+          expect(game.winner?(game.board.player2)).to eql(true)
+        end
+        it 'Check win second diagonal for player 2' do
+          game.start_board('Player 1', 'Player 2', :X, :O)
+          game.board.set_marker(3, game.board.player2.marker)
+          game.board.set_marker(5, game.board.player2.marker)
+          game.board.set_marker(7, game.board.player2.marker)
+          expect(game.winner?(game.board.player2)).to eql(true)
+        end
+      end
+    end
+  end
+end
